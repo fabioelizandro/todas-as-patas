91281cfc7f4e8223d3076f4e2843b3f5aa02db0b
Fábio Lemos Elizandro <fabio@elizandro.com.br>
2014-09-15 23:15:14 -0300

    Configuração para deploy

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -3,14 +3,24 @@
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Yaml\Yaml;
 
-if (getenv("SYMFONY_PARAMETERS_FILE") === 'parameters.php') {
+if (getenv('DATABASE_URL')) {
+    $dbUrl = getenv('DATABASE_URL');
+    $parts = parse_url($dbUrl);
 
-    $parameters = Yaml::parse(file_get_contents(__DIR__ . "/parameters.yml.dist"));
-
-    foreach ($parameters['parameters'] as $parameter => $defaultValue) {
-        /* @var $container ContainerInterface */
-        $container->setParameter($parameter, getenv($parameter));
-    }
+    $container->setParameter('sylius.database.driver', 'pdo_pgsql');
+    $container->setParameter('sylius.database.host', $parts['host']);
+    $container->setParameter('sylius.database.name', trim($parts['path'], '/'));
+    $container->setParameter('sylius.database.user', $parts['user']);
+    $container->setParameter('sylius.database.password', $parts['pass']);
+    $container->setParameter('sylius.database.port', $parts['port']);
 }
 
+if (getenv("SYMFONY_ENV_VARS")) {
 
+    $parameters = Yaml::parse(file_get_contents(__DIR__ . "/parameters_env_map.yml"));
+
+    foreach ($parameters['parameters_env_map'] as $parameter => $envKey) {
+        /* @var $container ContainerInterface */
+        $container->setParameter($parameter, getenv($envKey));
+    }
+}
