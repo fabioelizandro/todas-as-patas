84044f2bfdc17ec14930d7bef95c71ecbcc11a0b
Fábio Lemos Elizandro <fabio@elizandro.com.br>
2014-10-15 23:53:24 -0300

    Correção da configuração firewalls para Oauth2

--- src/ByteinCoffee/UserBundle/Controller/SecurityController.php
+++ src/ByteinCoffee/UserBundle/Controller/SecurityController.php
@@ -68,9 +68,7 @@ class SecurityController extends ContainerAware
      */
     protected function renderLogin(array $data)
     {
-        $template = sprintf('FOSUserBundle:Security:login.html.%s', $this->container->getParameter('fos_user.template.engine'));
-
-        return $this->container->get('templating')->renderResponse($template, $data);
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Security:login.html.twig', $data);
     }
 
     public function checkAction()
